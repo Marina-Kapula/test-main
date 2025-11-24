d9983ffb3ec112d5c265e28e93272fac866ed6bd
Maryna Kapula <maryna.kapula@gmail.com>
2025-11-24 14:17:32 +0200

    24124214

--- src/servise/persons.js
+++ src/servise/persons.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-const baseUrl = 'https://beckend-main.onrender.com/'
+const baseUrl = 'https://beckend-main.onrender.com/api/persons'  // ← должно быть так!
 
 const getAll = () => axios.get(baseUrl).then(res => res.data)
 const create = (newPerson) => axios.post(baseUrl, newPerson).then(res => res.data)
-// При необходимости — update, remove, etc.
+// update, remove – аналогично
 
 export default { getAll, create }
